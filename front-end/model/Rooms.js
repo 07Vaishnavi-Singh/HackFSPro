5c95db370c49de89549aff7d0ee151108b4610ad
sudarsan <s_r@mt.iitr.ac.in>
2023-06-17 10:52:30 +0530

    Boolean field added

--- front-end/model/Rooms.js
+++ front-end/model/Rooms.js
@@ -1,37 +1,32 @@
-const mongoose = require("mongosse");
+const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 
 const roomSchemaPublic = new Schema({
-   
-    name :{
-        type : String
-    },
+  name: {
+    type: String,
+  },
+  chain: {
+    type: String,
+  },
+  tokenType: {
+    type: String,
+  },
+  contractAddress: {
+    type: String,
+  },
+  interest: {
+    type: String,
+  },
+  capacity: {
+    type: Number,
+    min: 2,
+    max: 20,
+  },
+  isPublic: {
+    type: Boolean,
+    default: true,
+  },
+});
 
-chain:{
-    type: String
-},
-    tokenType:{
-        type: String
-    },
 
-    contractAddress:{
-        type : String
-    },
-
-
-    interest :{
-        trype: String
-    },
-
-
-    capacity:{
-        type : Number,
-        min:2,
-        max:20
-    }
-
-    // isPublic()
-
-  });
-  
