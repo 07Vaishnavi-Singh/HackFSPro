eb03911bec8fd683e416c34c02ff803717b2a181
sudarsan <s_r@mt.iitr.ac.in>
2023-06-17 14:51:05 +0530

    Flow fix #1

--- front-end/src/pages/RoomCreation.jsx
+++ front-end/src/pages/RoomCreation.jsx
@@ -1,12 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Titlebar from "../Components/Titlebar";
 import Features from "../Components/Features";
 import AvailableRooms from "../Components/AvailableRooms";
 import RoomForm from "../Components/RoomCreationForm";
+import Aboutus from "../Components/Aboutus";
 import "../styles/App.css";
 
 function RoomCreation({ isMetamaskConnected }) {
   const [showForm, setShowForm] = useState(false);
+  const featuresRef = useRef(null);
+  const aboutRef = useRef(null);
+
+  const scrollToFeatures = () => {
+    featuresRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+
+  const scrollToAboutUs = () => {
+    aboutRef.current.scrollIntoView({ behavior: "smooth" });
+  };
 
   const handleOpen = () => {
     setShowForm(true);
@@ -19,11 +30,26 @@ function RoomCreation({ isMetamaskConnected }) {
   return (
     <div id="dashboard-page">
       <div id="dashboard-container-1">
-        <Titlebar isMetamaskConnected={isMetamaskConnected} />
-        {showForm ? <RoomForm onButtonClick={handleClose} /> : <AvailableRooms onButtonClick1={handleOpen} />}
+        <Titlebar
+          id="home"
+          onButtonClick={scrollToFeatures}
+          onButtonClick2={scrollToAboutUs}
+          
+        />
+        {showForm ? (
+          <RoomForm onButtonClick={handleClose} />
+        ) : (
+          <AvailableRooms onButtonClick1={handleOpen} />
+        )}
       </div>
-      <div id="dashboard-container-2">
+      <div id="dashboard-container-2" >
+        <div id="Features" ref={featuresRef}>
         <Features />
+        </div>
+        
+      </div>
+      <div id="About" ref={aboutRef}>
+        <Aboutus />
       </div>
     </div>
   );
